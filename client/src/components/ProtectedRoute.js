9e4c0b62dd4d3b7f0022120bb6c50ef7b1c67ae2
Farhan Haider <farhanhaider624@gmail.com>
2024-06-30 13:24:00 +0530

    exam-list, instructions,result page, timer add

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -16,7 +16,7 @@ const ProtectedRoute = ({ children }) => {
   const userMenu = [
     {
       title: "Home",
-      paths: ["/"],
+      paths: ["/", "/user/write-exam"],
       icon: <i class="ri-home-line"></i>,
       onClick: () => navigate("/"),
     },
@@ -45,38 +45,38 @@ const ProtectedRoute = ({ children }) => {
 
   const adminMenu = [
     {
-        title: "Home",
-        paths: ["/"],
-        icon: <i className="ri-home-line"></i>,
-        onClick: () => navigate("/"),
+      title: "Home",
+      paths: ["/", "/user/write-exam"],
+      icon: <i className="ri-home-line"></i>,
+      onClick: () => navigate("/"),
+    },
+    {
+      title: "Exams",
+      paths: ["/admin/exams", "/admin/exams/add", "/admin/exams/edit/:id"],
+      icon: <i className="ri-file-list-line"></i>,
+      onClick: () => navigate("/admin/exams"),
+    },
+    {
+      title: "Reports",
+      paths: ["/admin/reports"],
+      icon: <i className="ri-bar-chart-line"></i>,
+      onClick: () => navigate("/admin/reports"),
+    },
+    {
+      title: "Profile",
+      paths: ["/admin/profile"],
+      icon: <i className="ri-user-line"></i>,
+      onClick: () => navigate("/profile"),
+    },
+    {
+      title: "Logout",
+      paths: ["/logout"],
+      icon: <i className="ri-logout-circle-r-line"></i>,
+      onClick: () => {
+        localStorage.removeItem("token");
+        navigate("/login");
       },
-      {
-        title: "Exams",
-        paths: ["/admin/exams", "/admin/exams/add", "/admin/exams/edit/:id"  ],
-        icon: <i className="ri-file-list-line"></i>,
-        onClick: () => navigate("/admin/exams"),
-      },
-      {
-        title: "Reports",
-        paths: ["/admin/reports"],
-        icon: <i className="ri-bar-chart-line"></i>,
-        onClick: () => navigate("/admin/reports"),
-      },
-      {
-        title: "Profile",
-        paths: ["/admin/profile"],
-        icon: <i className="ri-user-line"></i>,
-        onClick: () => navigate("/profile"),
-      },
-      {
-        title: "Logout",
-        paths: ["/logout"],
-        icon: <i className="ri-logout-circle-r-line"></i>,
-        onClick: () => {
-          localStorage.removeItem("token");
-          navigate("/login");
-        },
-      },
+    },
   ];
 
   const getUserData = async () => {
@@ -96,20 +96,34 @@ const ProtectedRoute = ({ children }) => {
         message.error(response.message);
       }
     } catch (error) {
+      navigate("/login");
       dispatch(HideLoading());
       message.error(error.message);
     }
   };
   useEffect(() => {
-    getUserData();
+    if (localStorage.getItem("token")) {
+      getUserData();
+    } else {
+      navigate("/login");
+    }
   }, []);
 
   const activeRoute = window.location.pathname;
 
   const getIsActiveOrNot = (paths) => {
-    if(paths.includes(activeRoute)) return true;
-    else{
-      if(activeRoute.includes("/admin/exams/edit") && paths.includes("/admin/exams")) return true;
+    if (paths.includes(activeRoute)) return true;
+    else {
+      if (
+        activeRoute.includes("/admin/exams/edit") &&
+        paths.includes("/admin/exams")
+      )
+      return true;
+      if (
+        activeRoute.includes("/user/write-exam") &&
+        paths.includes("/user/write-exam")
+      )
+      return true;
     }
     return false;
   };
@@ -156,9 +170,7 @@ const ProtectedRoute = ({ children }) => {
               <div className="text-xl underline">{user?.name}</div>
             </div>
           </div>
-          <div className="content">
-            {children}
-          </div>
+          <div className="content">{children}</div>
         </div>
       </div>
     </div>
---
+++ client/src/pages/user/WriteExam/index.jsx
@@ -0,0 +1,232 @@
+import React, { useEffect, useState } from "react";
+import { message, Button } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import { getExamById } from "../../../apicalls/exams";
+import { useDispatch } from "react-redux";
+import { ShowLoading, HideLoading } from "../../../redux/loaderSlice";
+import Instructions from "./Instructions";
+import { set } from "mongoose";
+
+const WriteExam = () => {
+  const [examData, setExamData] = React.useState(null);
+  const [questions = [], setQuestions] = React.useState([]);
+  const [selectedQuestionIndex, setSelectedQuestionIndex] = React.useState(0);
+  const [selectedOptions, setSelectedOptions] = React.useState({});
+  const [result = {}, setResult] = React.useState({});
+  const params = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [view, setView] = useState("instructions");
+  const [secondsLeft = 0, setSecondsLeft] = useState(0);
+  const [timeUp, setTimeUp] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const getExamData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await getExamById({
+        examId: params.id,
+      });
+      dispatch(HideLoading());
+      if (response.success) {
+        setQuestions(response.data.questions);
+        setExamData(response.data);
+        setSecondsLeft(response.data.duration);
+      } else {
+        message.error(response.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  const calculateResult = () => {
+    let correctAnswers = [];
+    let wrongAnswers = [];
+
+    questions.forEach((question, index) => {
+      if (question.correctOption === selectedOptions[index]) {
+        correctAnswers.push(question);
+      } else {
+        wrongAnswers.push(question);
+      }
+    });
+    let verdict = "Pass";
+    if (correctAnswers.length < examData.passingMarks) {
+      verdict = "Fail";
+    }
+    setResult({
+      correctAnswers,
+      wrongAnswers,
+      verdict,
+    });
+    setView("result");
+  };
+
+  const startTimer = () => {
+    let totalSeconds = examData.duration;
+    const intervalId = setInterval(() => {
+      if (totalSeconds > 0) {
+        totalSeconds=totalSeconds-1;
+        setSecondsLeft(totalSeconds);
+      } else {
+        setTimeUp(true);
+      }
+    }, 1000);
+    setIntervalId(intervalId);
+  };
+
+  useEffect(() => {
+    if (timeUp) {
+      clearInterval(intervalId);
+      calculateResult();
+    }
+  }, [timeUp]);
+
+  useEffect(() => {
+    if (params.id) {
+      getExamData();
+    }
+  }, []);
+  return (
+    examData && (
+      <div>
+        <div className="divider"></div>
+        <h1 className="text-center font-wt-none mt-0">{examData.name}</h1>
+        <div className="divider"></div>
+        {view === "instructions" && (
+          <Instructions
+            examData={examData}
+            view={view}
+            setView={setView}
+            startTimer={startTimer}
+          />
+        )}
+
+        {view === "questions" && (
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <h1 className="text-2xl">
+                {selectedQuestionIndex + 1} :{" "}
+                {questions[selectedQuestionIndex].name}
+              </h1>
+              <div className="timer">
+                <span className="text-2xl">{secondsLeft}</span>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              {Object.keys(questions[selectedQuestionIndex].options).map(
+                (option, index) => {
+                  return (
+                    <div
+                      className={`flex gap-2 flex-col ${
+                        selectedOptions[selectedQuestionIndex] === option
+                          ? "selected-option"
+                          : "option"
+                      }`}
+                      key={index}
+                      onClick={() => {
+                        setSelectedOptions({
+                          ...selectedOptions,
+                          [selectedQuestionIndex]: option,
+                        });
+                      }}
+                    >
+                      <h1 className="text-md mt-0">
+                        {option} :{" "}
+                        {questions[selectedQuestionIndex].options[option]}
+                      </h1>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+
+            <div className="flex justify-between">
+              {selectedQuestionIndex > 0 && (
+                <button
+                  className="primary-outlined-btn"
+                  onClick={() => {
+                    setSelectedQuestionIndex(selectedQuestionIndex - 1);
+                  }}
+                >
+                  Previous
+                </button>
+              )}
+
+              {selectedQuestionIndex < questions.length - 1 && (
+                <Button
+                  type="primary"
+                  className="primary-contained-btn"
+                  onClick={() => {
+                    setSelectedQuestionIndex(selectedQuestionIndex + 1);
+                  }}
+                >
+                  Next
+                </Button>
+              )}
+              {selectedQuestionIndex === questions.length - 1 && (
+                <Button
+                  type="primary"
+                  className="primary-contained-btn"
+                  onClick={() => {
+                    setTimeUp(true);
+                    clearInterval(intervalId);
+                    calculateResult();
+                  }}
+                >
+                  Submit
+                </Button>
+              )}
+            </div>
+          </div>
+        )}
+
+        {view === "result" && (
+          <div className="flex items-center mt-5 justify-center">
+            <div className="flex flex-col gap-2 result">
+              <h1 className="text-2xl">RESULT</h1>
+              <div className="marks">
+                <h1 className="text-md">Total Marks: {examData.totalMarks}</h1>
+                <h1 className="text-md">
+                  Passing Marks: {examData.passingMarks}
+                </h1>
+                <h1 className="text-md">
+                  Obtained Marks: {result.correctAnswers.length}
+                </h1>
+                <h1 className="text-md">
+                  Wrong Answers: {result.wrongAnswers.length}
+                </h1>
+                <h1 className="text-md">VERDICT: {result.verdict}</h1>
+              </div>
+            </div>
+            <div className="lottie-animation">
+              {result.verdict === "Pass" && (
+                <dotlottie-player
+                  src="https://lottie.host/4b48ca31-6953-4780-b1ce-7a1c8985a5bc/XwvT1PTqqd.json"
+                  background="transparent"
+                  speed="1"
+                  loop
+                  autoplay
+                ></dotlottie-player>
+              )}
+
+              {result.verdict === "Fail" && (
+                <dotlottie-player
+                  src="https://lottie.host/66659a60-8277-4015-b8be-9cba461aa068/Zb9jQhjE8b.json"
+                  background="transparent"
+                  speed="1"
+                  loop
+                  autoplay
+                ></dotlottie-player>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  );
+};
+
+export default WriteExam;
